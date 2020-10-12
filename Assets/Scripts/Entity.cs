0574533ff2476cc830837ee7ab692a86e991156b
nuppington-bit <harveyxthomison@gmail.com>
2020-10-12 10:21:14 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/Tidal-Members/SplitBullet""
    This reverts commit 41ce2e44e512df00c2d13e7483ae00b8d5ebc6f3.

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     public int health = 100;
+    public void Damage(int damageAmount)
+    {
+        //subtract damage amount when Damage function is called
+        health -= damageAmount;
+
+        //Check if health has fallen below zero
+        if (health <= 0) 
+        {
+            //if health has fallen below zero, destroy it 
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
-public class WeaponBase : MonoBehaviour
+public class WeaponBase
 {
     public enum HitType
     {
@@ -11,20 +11,50 @@ public class WeaponBase : MonoBehaviour
         Projectile
     }
     public string localizationKey = "#weapon.base";
-    public int health = 100;
     public HitType type = HitType.HitScan;
     public SharedEnums.HoldType holdType = SharedEnums.HoldType.OneHanded;
     public int damage = 10;
-    public int hitScanRange = -1;
-    public Sprite bucketIcon;
-    public Sprite selectedIcon;
-    public Sprite handSprite;
-    public Sprite[] fireAnim;
+    public float hitScanRange = 50f;
+    public float hitForce = 100f;
+    public float weaponCooldown = 0.5f;
+    public Sprite bucketIcon = Resources.Load<Sprite>("Sprites/BucketIcons/pistol");
+    public Sprite selectedIcon = Resources.Load<Sprite>("Sprites/SelectedIcons/pistol");
+    public Sprite handSprite = Resources.Load<Sprite>("Sprites/Weapons/glock");
+    public Color lightColor = Color.white;
+    private Player player;
+    public WeaponBase(Player ply)
+    {
+        player = ply;
+    }
     public void BaseFire()
     {
+        if(player.cooldown)
+            return;
         switch(type)
         {
             case HitType.HitScan:
+                Vector3 rayOrigin = player.lookScript.weaponLight.transform.position;
+                RaycastHit hit;
+                // Check if our raycast has hit anything
+                if (Physics.Raycast (rayOrigin, player.lookScript.weaponLight.transform.forward, out hit, hitScanRange))
+                {
+                    // Get a reference to a health script attached to the collider we hit
+                    Entity health = hit.collider.GetComponent<Entity>();
+
+                    // If there was a health script attached
+                    if (health != null)
+                    {
+                        // Call the damage function of that script, passing in our gunDamage variable
+                        health.Damage (damage);
+                    }
+
+                    // Check if the object we hit has a rigidbody attached
+                    if (hit.rigidbody != null)
+                    {
+                        // Add force to the rigidbody we hit, in the direction from which it was hit
+                        hit.rigidbody.AddForce (-hit.normal * hitForce);
+                    }
+                }
                 break;
             default:
                 Debug.Log("Unimplemented HitType fired!");
@@ -33,15 +63,15 @@ public class WeaponBase : MonoBehaviour
     }
     public virtual void Fire()
     {
-
+        BaseFire();
     }
     public void BaseEquip()
     {
-        GetComponentInParent<Player>().holdState = holdType;
+        player.holdState = holdType;
     }
     public virtual void Equip()
     {
-
+        BaseEquip();
     }
     public virtual void Holster()
     {
